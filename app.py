9582c4217f9e083cba67c2b6d1d604391d2fe38e
MuhammadSaud60 <muhammadsaud1000@gmail.com>
2025-04-16 09:45:51 +0500

    Updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ brands = pd.read_csv('brands.csv')
 
 
 
-st.title('No Thanks')
+st.title('NO THANKS')
 st.subheader('Search or Select from dropdown a brand/Company')
 
 dropdown = st.selectbox('All Brands',options=[""] + brands['id'])
@@ -25,6 +25,11 @@ st.markdown(
             border: 3px solid red;
             background-color: Scarlet;
             padding: 10px;
+            border-radius: 16px;
+            }
+
+            .details h3{
+                color: green;
             }
         </style>
 
@@ -43,19 +48,25 @@ if st.button("Search"):
         result = brands[brands['name'].str.strip().str.contains(user_input, case=False, na=False)]
 
         if not result.empty:
-            html = "<div class='details' style='width:100%; margin-top:20px; border:3px solid grey; padding:10px;'>"
+            html1 = "<div class='details' style='width:100%; margin-top:20px; border:3px solid grey; padding:10px;'>"
+            
 
             for _, row in result.iterrows():
-                html += f"<h4>{row['name']}</h4>"
-                html += f'<img src="{row["logo_url"]}" width="80" style="margin-bottom:10px;"><br>'
-                html += f'<p>{row["description"]}</p>'
-                html += f'<h4 style="color: red">{row["status"]}</h4><hr>'
+                html1 += f"<h4>{row['name']}</h4>"
+                html1 += f'<h3>Logo</h3>'
+                html1 += f'<img src="{row["logo_url"]}" width="80" style="margin-bottom:10px;"><br>'
+                html1 += f'<br>'
+                html1 += f'<h3>Reason</h3>'
+                html1 += f'<p>{row["description"]}</p>'
+                html1 += f'<br>'
+                html1 += f'<h4 style="color: red">{row["status"]}</h4><hr>'
 
-            html += "</div>"
+            
+            html1 += "</div>"
 
-            st.markdown(html, unsafe_allow_html=True)
+            st.markdown(html1, unsafe_allow_html=True)
 
         else:
-            st.markdown("<div style='width:100%; margin-top:20px; border:3px solid red; padding:10px;'>❌ No matching results found.</div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='width:100%; margin-top:20px; border:3px solid red; padding:10px;'>Please use search: Some brands may not show up unless you include spaces in the name. For example, try '5 Gum' instead of '5gum.</div>", unsafe_allow_html=True)
     else:
         st.warning("Please enter a search term or select from dropdown.")
